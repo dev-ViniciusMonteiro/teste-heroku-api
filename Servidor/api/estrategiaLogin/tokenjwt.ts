b3ae78d60a72f75021e8db1cb8f6d8e717d3faa7
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-07-13 09:55:55 -0300

    teste6

--- Servidor/api/estrategiaLogin/tokenjwt.ts
+++ Servidor/api/estrategiaLogin/tokenjwt.ts
@@ -4,6 +4,6 @@ export function criaTokenJWT(login:{id:number, senha:string, usuarioId:number}){
     const payload = {
         id: login.id
     }
-    const token = jwt.sign(payload, "MwRs2t6n02UtPUJ2WKXOIgXjIYZCt0L1qykUwHrqZBoUH0aFDnA5LWSyFarMWjwqgRqZB6F9osgu8pGu++2hRZzN603ItDuRYOvcHRq25Y0My/TadRWa6XCaUCaLYLiCMjWakt5alvLr4N47mK7sZw+SpATjH2E3mtgp1dDIDnQFVu4TQmApMkU6tNKAF9k08prO2nq3/3A99YpixbPut9I9o6ushHm9z2DQkgehVFRS2Fv5KIoFOwFumpi1MngXoGBelnAs0MCYVu72Ie2W/3Tqeh0QrP6bHoDGqHGyMI6txXw8nO9Hf9e6f7Iy+Nf/Gf3MjuUrVq15C9rYVMt6Aw==",{expiresIn: '60m'})
+    const token = jwt.sign(payload, process.env.CHAVE_JWT,{expiresIn: '60m'})
     return token;
 }
--- Servidor/api/server.ts
+++ Servidor/api/server.ts
@@ -1,7 +1,6 @@
 import app  from './app'
 import db from './models'
 const logger = require('./config/logger')
-require('dotenv').config()
 
 if(process.env.NODE_ENV == undefined){
     db.sequelize.sync().then(()=>{
@@ -11,7 +10,7 @@ if(process.env.NODE_ENV == undefined){
         })
     })
 }else{
-    app.listen((process.env.PORT || 8877),()=>{
+    app.listen(8877,()=>{
         logger.log('info',`Servidor rodando na porta: 3000`)
        console.log("APP ESCUTANDO NA PORTA 3000")
        })
