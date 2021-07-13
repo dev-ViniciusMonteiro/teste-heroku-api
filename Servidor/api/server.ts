faa6487d6069f2141b296338421d36ff849727ff
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-07-13 10:17:46 -0300

    voltando commits

--- Servidor/api/server.ts
+++ Servidor/api/server.ts
@@ -4,7 +4,7 @@ const logger = require('./config/logger')
 
 if(process.env.NODE_ENV == undefined){
     db.sequelize.sync().then(()=>{
-     app.listen((process.env.PORT || 8877),()=>{
+     app.listen(8877,()=>{
          logger.log('info',`Servidor rodando na porta: 3000`)
         console.log("APP ESCUTANDO NA PORTA 3000")
         })
