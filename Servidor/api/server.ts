ba2549130f806da10eefd9ed749eff94df2f161c
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-07-13 09:29:58 -0300

    port

--- Servidor/api/server.ts
+++ Servidor/api/server.ts
@@ -1,16 +1,17 @@
 import app  from './app'
 import db from './models'
 const logger = require('./config/logger')
+require('dotenv').config()
 
 if(process.env.NODE_ENV == undefined){
     db.sequelize.sync().then(()=>{
-     app.listen(8877,()=>{
+     app.listen((process.env.PORT || 8877),()=>{
          logger.log('info',`Servidor rodando na porta: 3000`)
         console.log("APP ESCUTANDO NA PORTA 3000")
         })
     })
 }else{
-    app.listen(8877,()=>{
+    app.listen((process.env.PORT || 8877),()=>{
         logger.log('info',`Servidor rodando na porta: 3000`)
        console.log("APP ESCUTANDO NA PORTA 3000")
        })
